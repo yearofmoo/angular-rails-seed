476d911db02acf5ff92817d64a84f730ace4e50c
Matias Niemelä <matias@yearofmoo.com>
2013-09-02 17:37:05 -0400

    removed extra files and migrations

--- config/initializers/embedly.rb
+++
@@ -1,4 +0,0 @@
-$embedly_api = Embedly::API.new({
-  :key => "6b7b41c00d0b407cac69c671878ded6b",
-  :user_agent => 'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)'
-})
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 AngularRailsApp::Application.routes.draw do
 
   #
-  # Admin Area
-  #
-  devise_for :admins
-  mount RailsAdmin::Engine => '/manage', :as => 'rails_admin'
-
-  #
   # API Area
   #
   namespace :api, defaults: {format: 'json'}  do
     namespace :v1 do
-      resource :contact, :controller => :contact
-      resources :articles
     end
   end
 
